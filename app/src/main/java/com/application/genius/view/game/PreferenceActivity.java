1a63cc16f6be3848d46b732a43836f03efb58b2b
Moises Almeida <moisesalmeida.dev@gmail.com>
2022-10-06 22:10:08 -0400

    created Practice and Preference (Activity)

---
+++ app/src/main/java/com/application/genius/view/game/PreferenceActivity.java
@@ -0,0 +1,16 @@
+package com.application.genius.view.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.application.genius.R;
+
+public class PreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+    }
+}
